88811a285af6b66d2055377102ad92a52590a0a3
liuyuju <liuyuju@forchange.tech>
2019-04-23 20:44:15 +0800

    add(post): detail

--- app/Models/Sellers.php
+++ app/Models/Sellers.php
@@ -21,6 +21,8 @@ class Sellers extends BaseModel
 
     protected $guarded = ["id"];
 
+    protected $casts = ["covers" => "json"];
+
     public function productions()
     {
         return $this->hasMany(Productions::class, "seller_id");
@@ -28,8 +30,8 @@ class Sellers extends BaseModel
 
     function getDistance($latitude, $longitude)
     {
-        return getDistance($latitude,$longitude,$this->latitude,$this->longitude);
-        dd(getDistance($latitude,$longitude,$this->latitude,$this->longitude));
+        return getDistance($latitude, $longitude, $this->latitude, $this->longitude);
+        dd(getDistance($latitude, $longitude, $this->latitude, $this->longitude));
 //        $sql = "select *,3959 * acos (cos ( radians({$latitude})) * cos( radians( {$this->latitude} ) )
 //      * cos( radians( {$this->longitude} ) - radians({$longitude}))  + sin ( radians({$latitude}) ) * sin( radians( {$this->tb_latitude} ) )
 //    ) distance from sellers";
